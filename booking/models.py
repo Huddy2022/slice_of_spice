8a3a40a5bc099e5888573fb02aba848436ca923a
Huddy2022 <mlvnhud@aol.com>
2023-03-08 22:24:27 +0000

    add database models for booking and table

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=35)
+    subject = models.TextField(default='')
+    message = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Table(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=35)
+    date = models.CharField(max_length=200)
+    time = models.CharField(max_length=12, default='')
+    phone = models.CharField(max_length=30, default='')
+    people = models.IntegerField(default='')
+    message = models.TextField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
