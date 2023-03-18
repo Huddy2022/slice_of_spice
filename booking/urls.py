ac20d532d38b31f86199e395dc842a66640b1245
Huddy2022 <mlvnhud@aol.com>
2023-03-18 16:52:59 +0000

    add navigation to anchor tags using urls.py and views.py

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 from booking import views
 
 urlpatterns = [
-    path('', views.index, name='Home')
+    path('', views.index, name='Home'),
+    path('home', views.home, name='home'),
+    path('menu', views.menu, name='menu'),
+    path('reservations', views.reservations, name='reservations'),
+    path('contact', views.contact, name='contact'),
+    path('gallery', views.gallery, name='gallery'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -4,3 +4,23 @@ from .models import Customer, Booking, Cancellation, Table
 
 def index(request):
     return render(request, 'index.html')
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
+def reservations(request):
+    return render(request, 'reservations.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
