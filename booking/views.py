8068923da2f4637cf7bc4b54cd0789755f7cb4ed
Huddy2022 <mlvnhud@aol.com>
2023-03-23 23:00:12 +0000

    add if statement for reservations.html and booking model

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from .models import Customer, Booking, Cancellation, Table
 
@@ -15,6 +17,15 @@ def menu(request):
 
 
 def reservations(request):
+    if request.method == "POST":
+        party = request.POST.get('party')
+        booking_date = request.POST.get('booking_date')
+        booking_time = request.POST.get('booking_time')
+
+        reservations = Booking(party=party, booking_date=booking_date, booking_time=booking_time)
+
+        reservations.save()
+
     return render(request, 'reservations.html')
 
 
