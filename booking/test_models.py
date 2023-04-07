fb04757af57f569b57735c86faac28b3fb5d9332
Huddy2022 <mlvnhud@aol.com>
2023-04-07 20:13:27 +0000

    add tests to my test_models.py and test them through the database.sqlite

--- booking/test_models.py
+++ booking/test_models.py
@@ -11,3 +11,55 @@ class ModelsTest(TestCase):
             username='testuser', password='testpass')
         # Create a customer linked to the user
         self.customer = Customer.objects.create(user=self.user)
+
+    def test_customer_str(self):
+        # Test the __str__ method of the Customer model
+        self.assertEqual(str(self.customer), 'testuser')
+
+    def test_table_str(self):
+        # Test the __str__ method of the Table model
+        table = Table.objects.create(table_number=1, capacity=2)
+        self.assertEqual(str(table), 'Table 1')
+
+    def test_booking_str(self):
+        # Test the __str__ method of the Booking model
+        table = Table.objects.create(table_number=1, capacity=2)
+        booking = Booking.objects.create(
+            customer=self.customer, table=table,
+            booking_date=date.today(), booking_time=time(hour=14, minute=30))
+        self.assertEqual(str(booking), 'testuser - 1')
+
+    def test_cancellation_str(self):
+        # Test the __str__ method of the Cancellation model
+        table = Table.objects.create(table_number=1, capacity=2)
+        booking = Booking.objects.create(
+            customer=self.customer, table=table, booking_date=date.today(),
+            booking_time=time(hour=14, minute=30))
+        cancellation = Cancellation.objects.create(
+            user=booking, message='test message', approved=True)
+        self.assertEqual(
+            str(cancellation), 'Cancellation for testuser - 1 - Approved: True'
+            )
+
+    def test_booking_unique_together(self):
+        # Test that the unique_together constraint is
+        # working in the Booking model
+        table = Table.objects.create(table_number=1, capacity=2)
+        booking1 = Booking.objects.create(
+            customer=self.customer, table=table, booking_date=date.today(),
+            booking_time=time(hour=14, minute=30))
+        with self.assertRaises(Exception):
+            booking2 = Booking.objects.create(
+                customer=self.customer, table=table, booking_date=date.today(),
+                booking_time=time(hour=14, minute=30))
+
+    def test_cancellation_delete_booking(self):
+        # Test that a booking is deleted when a cancellation is approved
+        table = Table.objects.create(table_number=1, capacity=2)
+        booking = Booking.objects.create(
+            customer=self.customer, table=table, booking_date=date.today(),
+            booking_time=time(hour=14, minute=30))
+        cancellation = Cancellation.objects.create(
+            user=booking, message='test message', approved=True)
+        with self.assertRaises(Booking.DoesNotExist):
+            Booking.objects.get(pk=booking.pk)
