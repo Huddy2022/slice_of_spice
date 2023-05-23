fc7af98f6e5ff9ffee1295b4b02cb8ee32e4e71f
Huddy2022 <mlvnhud@aol.com>
2023-05-23 21:19:03 +0000

    final commit for deployment

--- slice_of_spice/settings.py
+++ slice_of_spice/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["sliceofspice.herokuapp.com", "localhost", "8000-huddy2022-sliceofspice-724f0pui3mj.ws-eu97.gitpod.io"]
 
